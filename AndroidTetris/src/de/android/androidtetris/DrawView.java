a1180f96842586846f89135a5d386af6aac4d218
gumartinm <gu.martinm@gmail.com>
2011-11-06 22:37:56 +0100

    Fix bottom boundary

--- AndroidTetris/src/de/android/androidtetris/DrawView.java
+++ AndroidTetris/src/de/android/androidtetris/DrawView.java
@@ -220,7 +220,7 @@ public class DrawView extends SurfaceView {
     	for(int x=0; x<4; x++)
     		for(int y=0; y<4; y++)
     			if(currentPiece.size[x][y] != Tile.NOCOLOR)
-    				if (newy + y > MAPHEIGHT)
+    				if (newy + y == MAPHEIGHT)
     					return true;
     	
     	//Check collisions
