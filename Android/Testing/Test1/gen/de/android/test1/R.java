ca6ec915f5c06be8a338a19be9822d18ad1f958b
gumartinm <gu.martinm@gmail.com>
2011-11-24 01:53:07 +0100

    First program with more than 1 activity. First steps with a http connection. Trying to use my WebService

--- Android/Testing/Test1/gen/de/android/test1/R.java
+++ Android/Testing/Test1/gen/de/android/test1/R.java
@@ -14,14 +14,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int ok=0x7f050000;
+        public static final int cancel_button=0x7f050005;
+        public static final int frameLayout1=0x7f050000;
+        public static final int frameLayout2=0x7f050003;
+        public static final int login_button=0x7f050004;
+        public static final int password=0x7f050002;
+        public static final int username=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int main2=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int button_ok=0x7f040002;
+        public static final int button_cancel=0x7f040006;
+        public static final int button_login=0x7f040004;
+        public static final int button_ok=0x7f040005;
         public static final int hello=0x7f040000;
+        public static final int password=0x7f040003;
+        public static final int username=0x7f040002;
     }
 }
--- Android/Testing/Test1/src/de/android/test1/Test1Activity.java
+++ Android/Testing/Test1/src/de/android/test1/Test1Activity.java
@@ -1,8 +1,15 @@
 package de.android.test1;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Test1Activity extends Activity {
     /** Called when the activity is first created. */
@@ -13,6 +20,36 @@ public class Test1Activity extends Activity {
     }
     
     public void onClickOk(View v) {
+    	Intent i = new Intent(Test1Activity.this, NextActivity.class);
+    	this.startActivity(i);
+    }
+    
+    public void onClickLogin(View v) {
+    	HttpClient httpclient = new DefaultHttpClient();
+    	
+    	//RESTful WebService
+    	HttpPost httppost = new HttpPost("http://10.208.184.41/userfront.php/api/51,32/0,5/gpsads.xml");
+    	
+    	EditText uname = (EditText)findViewById(R.id.username);
+        String username = uname.getText().toString();
         
+        EditText pword = (EditText)findViewById(R.id.password);
+        String password = pword.getText().toString();
+        
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+        nameValuePairs.add(new BasicNameValuePair("username", username));
+        nameValuePairs.add(new BasicNameValuePair("password", password));
+        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+
+
+
+
+    	Intent i = new Intent(Test1Activity.this, NextActivity.class);
+    	this.startActivity(i);
+    }
+    
+    public void onClickCancel(View v) {
+    	finish();
     }
 }
