db69b849b5d73afe19cf7eeca96d7202809cf743
gumartinm <gu.martinm@gmail.com>
2011-12-14 04:11:49 +0100

    Finally I will not use AsyncTask for the log in Activity because must not be an asynchronous activity.

---
+++ Android/Testing/Test2/src/de/android/test2/NextActivity.java
@@ -0,0 +1,17 @@
+package de.android.test2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+
+public class NextActivity extends Activity {
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CookieSyncManager.createInstance(this);
+        String myCookie = CookieManager.getInstance().getCookie("192.168.1.34/userfront.php");
+        setContentView(R.layout.main2);
+    }
+}
