b9c0d53ed34d1b098b8335f28b05570899dda623
gu.martinm@gmail.com <gu.martinm@gmail.com>
2014-03-26 20:13:01 +0100

    No time for comments

--- Allgemeines/UNICODECollation/src/de/example/collation/CollationExample.java
+++ Allgemeines/UNICODECollation/src/de/example/collation/CollationExample.java
@@ -19,7 +19,14 @@ public class CollationExample {
         final Collator es_ESCollator = Collator.getInstance(new Locale("es","ES"));
         final Collator fr_FRCollator = Collator.getInstance(Locale.FRANCE);
 
+        // String.compareTo and String.compareToIgnoreCase are using UNICODE
+        // values directly
+        //final String test = "GUS";
+        //test.compareToIgnoreCase("gus");
+
+        es_ESCollator.setStrength(Collator.TERTIARY);
         Collections.sort(wordsListES, es_ESCollator);
+        fr_FRCollator.setStrength(Collator.TERTIARY);
         Collections.sort(wordsListFR, fr_FRCollator);
 
         System.out.println("Words list ES: " + wordsListES);
