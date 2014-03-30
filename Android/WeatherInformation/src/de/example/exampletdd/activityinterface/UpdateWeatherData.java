46836222d11b6a14a7eadb56792ebe1ff54701ed
gu.martinm@gmail.com <gu.martinm@gmail.com>
2014-03-30 19:33:50 +0200

    No time for comments

--- Android/WeatherInformation/src/de/example/exampletdd/activityinterface/UpdateWeatherData.java
+++
@@ -1,8 +0,0 @@
-package de.example.exampletdd.activityinterface;
-
-import de.example.exampletdd.model.WeatherData;
-
-public interface UpdateWeatherData {
-
-    public void updateWeatherData(WeatherData weatherData);
-}
