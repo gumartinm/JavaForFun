7a8b679aba9f1db358913596d7b5e85366613736
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2015-09-09 01:39:35 +0200

    SpringJava REST: CarController, commit related to
    https://github.com/gumartinm/JavaScriptForFun/commit/ee9f818b18022b98b185cc1938a0302381c89329

--- SpringJava/REST/src/main/java/rest/CarController.java
+++ SpringJava/REST/src/main/java/rest/CarController.java
@@ -33,6 +33,13 @@ public class CarController {
     @RequestMapping(value = "/{id}", produces = { "application/json" }, method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public Car car(@PathVariable("id") long id) {
+        try {
+            Thread.sleep(10000);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+
         return new Car(counter.incrementAndGet(), String.format(template, id));
     }
 }
