7b0ccef5f99ef6d203bfad141d30a620e61112f7
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-01-03 22:12:33 +0100

    Spring REST: Jackson requires default constructor

--- SpringJava/REST/src/main/java/de/spring/webservices/rest/Car.java
+++ SpringJava/REST/src/main/java/de/spring/webservices/rest/Car.java
@@ -5,6 +5,12 @@ public class Car {
     private final Long id;
     private final String content;
 
+    // Required by Jackson :/
+    public Car() {
+    	this.id = null;
+    	this.content = null;
+    }
+    
     public Car(Long id, String content) {
         this.id = id;
         this.content = content;
--- SpringJava/REST/src/test/java/de/spring/webservices/rest/CarControllerIntegrationTest.java
+++ SpringJava/REST/src/test/java/de/spring/webservices/rest/CarControllerIntegrationTest.java
@@ -38,7 +38,7 @@ public class CarControllerIntegrationTest {
 	@Test
 	public void testWhenGetAllCarsThenRetrieveJsonValues() throws Exception {
 		mockMvc.perform(get("/api/cars/")
-				.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.accept(MediaType.APPLICATION_JSON_UTF8))
 		
 		.andExpect(status().isOk())
 		.andExpect(jsonPath("$[0].id", any(Integer.class)))
@@ -47,33 +47,33 @@ public class CarControllerIntegrationTest {
 		.andExpect(jsonPath("$[1].id", any(Integer.class)))
 		.andExpect(jsonPath("$[2].content", is("Car: 3")))
 		.andExpect(jsonPath("$[2].id", any(Integer.class)))
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
 	}
 	
 	@Test
 	public void testWhenGetOneCarThenRetrieveJsonValue() throws Exception {
 		mockMvc.perform(get("/api/cars/{id}", 1L)
-				.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.accept(MediaType.APPLICATION_JSON_UTF8))
 	
 		.andExpect(status().isOk())
 		.andExpect(jsonPath("id", any(Integer.class)))
 		.andExpect(jsonPath("content", is("Car: 1")))
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
 	}
 	
 	@Test
 	public void testWhenCreateNewCarThenRetrieveJsonValue() throws Exception {
 		Car car = new Car(2L, "nothing");
 		mockMvc.perform(post("/api/cars/")
-				.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.content(asJsonString(car))
-				.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.accept(MediaType.APPLICATION_JSON_UTF8))
 		
 		.andExpect(status().isCreated())
 		.andExpect(jsonPath("id", any(Integer.class)))
 		.andExpect(jsonPath("content", is("Car: 1")))
 		.andExpect(header().string(HttpHeaders.LOCATION, "/api/cars/1"))
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
 	}
 	
 	private static String asJsonString(final Object obj) throws JsonProcessingException {
