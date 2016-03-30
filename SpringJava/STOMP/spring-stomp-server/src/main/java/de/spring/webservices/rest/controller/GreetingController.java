205daa02a83be5e202dc2f92c9f09a87a2f279da
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-03-30 02:12:06 +0200

    Sprint STOMP: simple broker (in memory broker)

--- SpringJava/STOMP/spring-stomp-server/src/main/java/de/spring/webservices/rest/controller/GreetingController.java
+++ SpringJava/STOMP/spring-stomp-server/src/main/java/de/spring/webservices/rest/controller/GreetingController.java
@@ -2,16 +2,32 @@ package de.spring.webservices.rest.controller;
 
 import java.time.LocalDateTime;
 
-import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class GreetingController {
 
-    @MessageMapping("/greeting")
-    public String handle(String greeting) {
-        return "[" + LocalDateTime.now() + ": " + greeting;
+	private SimpMessagingTemplate template;
+
+    @Autowired
+    public GreetingController(SimpMessagingTemplate template) {
+        this.template = template;
     }
-	
+
+//    @MessageMapping("/greeting")
+//    public String handle(String greeting) {
+//        return "[" + LocalDateTime.now() + ": " + greeting;
+//    }
+
+	@RequestMapping(path="/greetings", method=RequestMethod.POST)
+    public void handle(String greeting) {
+		String text = "[" + LocalDateTime.now() + "]:" + greeting;
+		this.template.convertAndSend("/topic/greeting", text);
+    }
+
 }
