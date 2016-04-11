3dd8df2fd2ed6e7be978e7f01f8c2b1e17b4d705
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-04-11 02:43:19 +0200

    Spring AOP and maven plugin

---
+++ SpringJava/AOP/SpringMavenWeaver/src/main/java/de/spring/example/TransactionManager.java
@@ -0,0 +1,48 @@
+package de.spring.example;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+
+@Aspect
+public class TransactionManager {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TransactionManager.class);
+    private static TransactionManager instance = new TransactionManager();
+    private DataSourceTransactionManager transactionManager;
+    private TransactionStatus transactionStatus;
+
+    //Why could you want to extend this class?
+    private TransactionManager() {
+    }
+
+    public static TransactionManager getInstance() {
+        return instance;
+    }
+
+    public void initTransaction()
+    {
+    	LOGGER.info("initTRANSACTION");
+        // transactionStatus = this.transactionManager.getTransaction(null);
+    }
+
+    public void rollbackTransaction()
+    {
+        this.transactionManager.rollback(this.transactionStatus);
+    }
+
+
+    public void commitTransaction()
+    {
+    	LOGGER.info("commitTRANSACTION");
+        // this.transactionManager.commit(this.transactionStatus);
+    }
+
+
+    /************************* Setters and getters *******************************************/
+    public void setTransactionManager(final DataSourceTransactionManager  transactionManager)
+    {
+        this.transactionManager = transactionManager;
+    }
+}
---
+++ SpringJava/AOP/SpringMavenWeaver/src/main/java/de/spring/example/web/Test.java
@@ -0,0 +1,40 @@
+package de.spring.example.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.spring.example.annotation.commitTransactional;
+import de.spring.example.annotation.initTransactional;
+
+
+public class Test {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
+
+    @initTransactional
+    public int myMethod()
+    {
+    	LOGGER.info("The Advice should be run before.");
+
+    	annotatedPrivateMethod();
+    	
+    	InnerTest innerTest = new InnerTest();
+    	innerTest.innerMethod();
+    	
+        return 666;
+    }
+
+    public class InnerTest {
+        @commitTransactional
+        public void innerMethod() {
+        	LOGGER.info("I am the inner class. The Advice should be run after."
+        			+ " NO NEED OF DECLARING Spring BEANS WHEN WEAVING!!!!");
+        }
+    }
+    
+    // IT WORKS WHEN WEAVING!!!
+    @initTransactional
+    private void annotatedPrivateMethod() {
+    	LOGGER.info("The Advice should be run before even with private methods because I AM WEAVING."
+    			+ " IT WORKS EVEN CALLING FROM METHOD OF THE SAME CLASS. It doesn't when using proxies AOP.");
+    }
+}
