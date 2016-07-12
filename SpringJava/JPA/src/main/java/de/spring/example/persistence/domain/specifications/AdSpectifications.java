19092372178bc55f782f54b07e131b1bdf52d353
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-07-12 03:09:12 +0200

    Spring JPA: Specifications, using lambda expressions

--- SpringJava/JPA/src/main/java/de/spring/example/persistence/domain/specifications/AdSpectifications.java
+++ SpringJava/JPA/src/main/java/de/spring/example/persistence/domain/specifications/AdSpectifications.java
@@ -2,10 +2,10 @@ package de.spring.example.persistence.domain.specifications;
 
 import java.time.LocalDate;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+//import javax.persistence.criteria.CriteriaBuilder;
+//import javax.persistence.criteria.CriteriaQuery;
+//import javax.persistence.criteria.Predicate;
+//import javax.persistence.criteria.Root;
 
 import org.springframework.data.jpa.domain.Specification;
 
@@ -13,30 +13,50 @@ import de.spring.example.persistence.domain.Ad;
 
 public class AdSpectifications {
 	
+//	public static Specification<Ad> createdToday() {
+//		return new Specification<Ad>() {
+//
+//			@Override
+//			public Predicate toPredicate(Root<Ad> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+//				final LocalDate date = LocalDate.now();
+//				
+//				return cb.equal(root.get("createdAt"), date);
+//			}
+//
+//		};
+//		
+//	}
+	
+	/**
+	 * The same using lambda expressions
+	 */
 	public static Specification<Ad> createdToday() {
-		return new Specification<Ad>() {
-
-			@Override
-			public Predicate toPredicate(Root<Ad> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				final LocalDate date = LocalDate.now();
-				
-				return cb.equal(root.get("createdAt"), date);
-			}
-
-		};
-		
+		return (root, query, cb) -> {
+			final LocalDate date = LocalDate.now();
+			
+			return cb.equal(root.get("createdAt"), date);
+        };
 	}
 	
+//	public static Specification<Ad> mobileImage(String image) {
+//		return new Specification<Ad>() {
+//
+//			@Override
+//			public Predicate toPredicate(Root<Ad> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+//				
+//				return cb.equal(root.get("adMobileImage"), image);
+//			}
+//
+//		};
+//		
+//	}
+	
+	/**
+	 * The same using lambda expressions
+	 */
 	public static Specification<Ad> mobileImage(String image) {
-		return new Specification<Ad>() {
-
-			@Override
-			public Predicate toPredicate(Root<Ad> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				
-				return cb.equal(root.get("adMobileImage"), image);
-			}
-
-		};
-		
+		return (root, query, cb) -> {
+			return cb.equal(root.get("adMobileImage"), image);
+        };
 	}
 }
