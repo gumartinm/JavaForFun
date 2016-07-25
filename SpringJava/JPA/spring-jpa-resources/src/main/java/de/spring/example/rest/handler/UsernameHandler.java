66c15a27766324af8386e239f29c3a88df25fd1f
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-07-25 21:14:54 +0200

    Spring JPA: no way of injecting bean in JPA entities

--- SpringJava/JPA/spring-jpa-resources/src/main/java/de/spring/example/rest/handler/UsernameHandler.java
+++ SpringJava/JPA/spring-jpa-resources/src/main/java/de/spring/example/rest/handler/UsernameHandler.java
@@ -1,6 +1,5 @@
 package de.spring.example.rest.handler;
 
-import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,12 +8,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import de.spring.example.context.UsernameThreadContext;
 
 public class UsernameHandler extends HandlerInterceptorAdapter {
-	private final UsernameThreadContext usernameThreadContext;
-	
-	@Inject
-	public UsernameHandler(UsernameThreadContext userNameThreadContext) {
-		this.usernameThreadContext = userNameThreadContext;
-	}
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -22,9 +15,9 @@ public class UsernameHandler extends HandlerInterceptorAdapter {
 		final String userName = request.getHeader(UsernameThreadContext.USERNAME_HEADER);
 		
 		if (userName != null) {
-			usernameThreadContext.setUsername(userName);
+			UsernameThreadContext.setUsername(userName);
 		} else {
-			usernameThreadContext.clearUsername();
+			UsernameThreadContext.clearUsername();
 		}
 		
 		return super.preHandle(request, response, handler);
@@ -34,6 +27,6 @@ public class UsernameHandler extends HandlerInterceptorAdapter {
 	public void afterCompletion(
 			HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-		usernameThreadContext.clearUsername();
+		UsernameThreadContext.clearUsername();
 	}
 }
