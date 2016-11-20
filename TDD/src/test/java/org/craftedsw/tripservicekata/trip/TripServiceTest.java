aa655fdb54443b843262fa34e1c7133bca24c7e2
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-11-20 22:42:43 +0100

    Sandro Mancuso, tripservicekata.

---
+++ TDD/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -0,0 +1,11 @@
+package org.craftedsw.tripservicekata.trip;
+
+import org.junit.Test;
+
+public class TripServiceTest {
+	
+	@Test public void
+	should_throw_an_exception_when_no_logged_user() {
+		
+	}
+}
