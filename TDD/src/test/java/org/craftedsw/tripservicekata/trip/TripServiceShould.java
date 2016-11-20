7a692c53946efdd32d792be3c34132ae3130f1f7
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-11-20 23:44:01 +0100

    We test the next branch.
    Remember: the assert must match the unit test method's name!!
    In this case, no trips must be returned.

--- TDD/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ TDD/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -1,5 +1,10 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import java.util.List;
+
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
@@ -8,6 +13,9 @@ public class TripServiceShould {
 	
 	private static final User GUEST = null;
 	private static final User UNUSED_USER = null;
+	private static final User REGISTERED_USER = new User();
+	private static final User ANOTHER_USER = new User();
+	private static final Trip TO_BRAZIL = new Trip();
 	private User loggedInUser;
 
 	@Test(expected=UserNotLoggedInException.class) public void
@@ -17,6 +25,23 @@ public class TripServiceShould {
 		loggedInUser = GUEST;
 		
 		tripService.getTripsByUser(UNUSED_USER);
+	}
+	
+	@Test public void
+	not_return_any_trips_when_users_are_not_friends() {
+		TesteableTripService tripService = new TesteableTripService();
+		
+		loggedInUser = REGISTERED_USER;
+		
+		User friend = new User();
+		friend.addFriend(ANOTHER_USER);
+		friend.addTrip(TO_BRAZIL);
+		
+		List<Trip> friendTrips = tripService.getTripsByUser(friend); 
+		// You must always begin writing the assert.
+		// Remember: the assert must match the unit test method's name!!
+		// In this case, no trips must be returned.
+		assertThat(friendTrips.size(), is(0));
 	}
 	
 	private class TesteableTripService extends TripService {
