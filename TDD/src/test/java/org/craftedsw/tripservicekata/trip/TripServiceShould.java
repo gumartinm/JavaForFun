9b741d910cdd7db4c3cb03b62cbb74671d69ef76
Gustavo Martin Morcuende <gustavo.martin@scmspain.com>
2016-11-24 01:50:37 +0100

    Improvement with fluent interface for our test

--- TDD/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ TDD/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -36,9 +36,10 @@ public class TripServiceShould {
 	
 	@Test public void
 	not_return_any_trips_when_users_are_not_friends() {	
-		User friend = new User();
-		friend.addFriend(ANOTHER_USER);
-		friend.addTrip(TO_BRAZIL);
+		User friend = UserBuilder.aUser()
+						.friendsWith(ANOTHER_USER)
+						.withTrips(TO_BRAZIL)
+						.build();
 		
 		List<Trip> friendTrips = tripService.getTripsByUser(friend); 
 		// You must always begin writing the assert.
@@ -48,18 +49,56 @@ public class TripServiceShould {
 	}
 	
 	@Test public void
-	return_friend_trips_when_users_are_friends() {		
-		User friend = new User();
-		friend.addFriend(loggedInUser);
-		friend.addFriend(ANOTHER_USER);
-		friend.addTrip(TO_BRAZIL);
-		friend.addTrip(TO_BERLIN);
+	return_friend_trips_when_users_are_friends() {
+		User friend = UserBuilder.aUser()
+						.friendsWith(ANOTHER_USER, loggedInUser)
+						.withTrips(TO_BRAZIL, TO_BERLIN)
+						.build();
 		
 		List<Trip> friendTrips = tripService.getTripsByUser(friend); 
 		// You must always begin writing the assert.
 		// Remember: the assert must match the unit test method's name!!
 		// In this case, no trips must be returned.
 		assertThat(friendTrips.size(), is(2));
+	}
+	
+	
+	public static class UserBuilder {
+		private User[] friends = new User[]{};
+		private Trip[] trips = new Trip[]{};
+		
+		public static UserBuilder aUser() {
+			return new UserBuilder();
+		}
+
+		public UserBuilder withTrips(Trip...trips) {
+			this.trips  = trips;
+			return this;
+		}
+
+		public UserBuilder friendsWith(User...friends) {
+			this.friends = friends;
+			return this;
+		}
+		
+		public User build() {
+			User user = new User();
+			addTripsTo(user);
+			addFriendsTo(user);
+			return user;
+		}
+
+		private void addFriendsTo(User user) {
+			for (User friend : friends) {
+				user.addFriend(friend);
+			}		
+		}
+
+		private void addTripsTo(User user) {
+			for (Trip trip : trips) {
+				user.addTrip(trip);
+			}
+		}
 	}
 	
 	private class TesteableTripService extends TripService {
