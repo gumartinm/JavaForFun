347102b96e59f4e05f2a1cfee9a7ede257931602
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-11-25 00:08:26 +0100

    isFriendsWith(user) method added to User

--- TDD/src/test/java/org/craftedsw/tripservicekata/user/UserShould.java
+++ TDD/src/test/java/org/craftedsw/tripservicekata/user/UserShould.java
@@ -1,5 +1,31 @@
 package org.craftedsw.tripservicekata.user;
 
-public class UserTest {
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
+import org.craftedsw.tripservicekata.UserBuilder;
+import org.junit.Test;
+
+public class UserShould {
+
+	private static final User JOHN = new User();
+	private static final User PAUL = new User();
+
+	@Test public void
+	inform_when_users_are_not_friends() {
+		User user = UserBuilder.aUser()
+						.friendsWith(JOHN)
+						.build();
+		
+		assertThat(user.isFriendsWith(PAUL), is(false));
+	}
+	
+	@Test public void
+	inform_when_users_are_friends() {
+		User user = UserBuilder.aUser()
+				.friendsWith(JOHN, PAUL)
+				.build();
+		
+		assertThat(user.isFriendsWith(PAUL), is(true));
+	}
 }
