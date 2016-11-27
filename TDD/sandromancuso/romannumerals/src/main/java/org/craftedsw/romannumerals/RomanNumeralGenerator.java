a9de6487a5362464bdcdc2c17b1de5451088e919
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-11-27 17:35:11 +0100

    Generalize code.
    From duplications we identify pattern and generalize our code.

--- TDD/sandromancuso/romannumerals/src/main/java/org/craftedsw/romannumerals/RomanNumeralGenerator.java
+++ TDD/sandromancuso/romannumerals/src/main/java/org/craftedsw/romannumerals/RomanNumeralGenerator.java
@@ -3,13 +3,12 @@ package org.craftedsw.romannumerals;
 public class RomanNumeralGenerator {
 
 	public static String romanFor(int decimal) {
-		if (decimal == 3) {
-			return "III";
+		String roman = "";
+		for (int i = 0; i < decimal; i++) {
+			roman  += "I";
 		}
-		if (decimal == 2) {
-			return "II";
-		}
-		return "I";
+		
+		return roman;
 	}
 
 }
