6ba63b5e937749b0637a00ec8c854557f9b99e49
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-12-04 19:32:51 +0100

    Clock test is green.
    Inline as much code as possible (getting ride of variables)
    We isolate and control the randomness in our code.
    No using spies, instead, we use private classes in our tests.

--- TDD/sandromancuso/bank/src/test/java/org/craftedsw/feature/ClockShould.java
+++ TDD/sandromancuso/bank/src/test/java/org/craftedsw/feature/ClockShould.java
@@ -3,17 +3,28 @@ package org.craftedsw.feature;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.time.LocalDate;
+
 import org.junit.Test;
 
 public class ClockShould {
 
 	@Test public void
 	return_todays_date_in_dd_MM_yyyy_format() {
-		Clock clock = new Clock();
+		Clock clock = new TestableClock();
 		
 		String date = clock.todayAsString();
 		
 		assertThat(date, is("04/12/2016"));
 	}
 
+	private class TestableClock extends Clock {
+		
+		@Override
+		protected LocalDate today() {
+			// We will control the random part of our code
+			// (no need of using spy)
+			return LocalDate.of(2016, 12, 4);
+		}
+	}
 }
