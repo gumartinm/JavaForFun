8eb1dc155ccc5a5279ceb780dd6d80f6c40016ae
Gustavo Martin Morcuende <gustavo.martin@scmspain.com>
2016-12-04 23:09:17 +0100

    Fixing coverage in Clock.today()
    My implementation solves the coverage issue (hopefully it is right)

---
+++ TDD/sandromancuso/bank/src/test/java/org/craftedsw/feature/ClockGusShould.java
@@ -0,0 +1,43 @@
+package org.craftedsw.feature;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.Test;
+
+public class ClockGusShould {
+	private static final DateTimeFormatter DD_MM_YYYY = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	@Test public void
+	return_todays_date_in_dd_MM_yyyy_format() {
+		ClockGus clock = spy(new ClockGus());
+		LocalDate today = today();
+		given(clock.formattedToday(today)).willReturn(today.format(DD_MM_YYYY));
+		
+		String date = clock.todayAsString();
+		
+		assertThat(date, is(today.format(DD_MM_YYYY)));
+		verify(clock, times(1)).formattedToday(today);
+	}
+	
+	@Test public void
+	return_date_in_dd_MM_yyyy_format() {
+		ClockGus clock = new ClockGus();
+		LocalDate today = today();
+		
+		String date = clock.formattedToday(today);
+		
+		assertThat(date, is(today.format(DD_MM_YYYY)));
+	}
+
+	private LocalDate today() {
+		return LocalDate.now();
+	}
+}
