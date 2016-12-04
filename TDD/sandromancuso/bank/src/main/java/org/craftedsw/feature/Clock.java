db44fcfef3b4ad57eb0e75a22e41bd26e9af811a
Gustavo Martin Morcuende <gustavo.martin@scmspain.com>
2016-12-04 18:47:15 +0100

    Clock test working but we need to control the random part.
    We need it in order to write tests that will work in any day.

--- TDD/sandromancuso/bank/src/main/java/org/craftedsw/feature/Clock.java
+++ TDD/sandromancuso/bank/src/main/java/org/craftedsw/feature/Clock.java
@@ -1,9 +1,14 @@
 package org.craftedsw.feature;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Clock {
 
 	public String todayAsString() {
-		throw new UnsupportedOperationException();
+		// We need to control this random stuff for our tests. :(
+		LocalDate today = LocalDate.now();
+		return today.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 	}
 
 }
