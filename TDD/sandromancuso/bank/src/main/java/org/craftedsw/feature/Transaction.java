e4a8ba4359af129fed8ba45d13313c160a3aa308
Gustavo Martin Morcuende <gustavo.martin@scmspain.com>
2016-12-04 18:31:12 +0100

    It is only left a test for addWithdrawal

--- TDD/sandromancuso/bank/src/main/java/org/craftedsw/feature/Transaction.java
+++ TDD/sandromancuso/bank/src/main/java/org/craftedsw/feature/Transaction.java
@@ -2,8 +2,39 @@ package org.craftedsw.feature;
 
 public class Transaction {
 
+	private final String date;
+	private final int amount;
+
 	public Transaction(String date, int amount) {
-		throw new UnsupportedOperationException();
+		this.date = date;
+		this.amount = amount;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + amount;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transaction other = (Transaction) obj;
+		if (amount != other.amount)
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		return true;
+	}
 }
