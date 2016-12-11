2d909d1322dc87eeb56795568d6fbc216fc0cb3b
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-12-11 23:45:15 +0100

    RxJavaCarControllerIntegrationTest, cleaning up imports

--- SpringJava/RxJava/web-services-spring-rxjava-server/src/test/java/de/spring/webservices/rest/controller/RxJavaCarControllerIntegrationTest.java
+++ SpringJava/RxJava/web-services-spring-rxjava-server/src/test/java/de/spring/webservices/rest/controller/RxJavaCarControllerIntegrationTest.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.data.domain.Page;
