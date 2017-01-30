515b84b2fe7823ad752d2a9c895076452d8780ca
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2017-01-30 02:44:19 +0100

    Using my own Schema Registry Server
    See: http://docs.spring.io/spring-cloud-stream/docs/current/reference/html/contenttypemanagement.html#_schema_registry_server

---
+++ SpringJava/Kafka/SpringCloudStream/spring-cloud-stream-kafka-registry-example/src/main/java/de/example/spring/kafka/Application.java
@@ -0,0 +1,17 @@
+package de.example.spring.kafka;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.stream.schema.server.EnableSchemaRegistryServer;
+
+@SpringBootApplication
+@EnableSchemaRegistryServer
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class);
+  }
+
+}
