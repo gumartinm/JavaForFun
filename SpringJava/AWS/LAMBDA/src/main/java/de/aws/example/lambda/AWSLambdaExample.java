18c50b3ef9acd41d10d0423ff52167bf323648bd
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2017-04-18 18:17:46 +0200

    AWS lambda improvements

--- SpringJava/AWS/LAMBDA/src/main/java/de/aws/example/lambda/AWSLambdaExample.java
+++ SpringJava/AWS/LAMBDA/src/main/java/de/aws/example/lambda/AWSLambdaExample.java
@@ -5,23 +5,31 @@ import org.apache.log4j.Logger;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+/**
+ * Check limits before using AWS lambda functions: http://docs.aws.amazon.com/lambda/latest/dg/limits.html
+ *
+ */
 public class AWSLambdaExample implements RequestHandler<Input, Output> {
 	private static final Logger LOGGER = Logger.getLogger(AWSLambdaExample.class);
 
 	@Override
 	public Output handleRequest(Input input, Context context) {
-		LOGGER.info("context getAwsRequestId:" + context.getAwsRequestId());
-		LOGGER.info("context getFunctionName:" + context.getFunctionName());
-		LOGGER.info("context getFunctionVersion:" + context.getFunctionVersion());
-		LOGGER.info("context getInvokedFunctionArn:" + context.getInvokedFunctionArn());
-		LOGGER.info("context getLogGroupName:" + context.getLogGroupName());
-		LOGGER.info("context getLogStreamName:" + context.getLogStreamName());
-		LOGGER.info("context getMemoryLimitInMB:" + context.getMemoryLimitInMB());
-		LOGGER.info("context getRemainingTimeInMillis:" + context.getRemainingTimeInMillis());
-		LOGGER.info("context getIdentityId:" + context.getIdentity().getIdentityId());
-		LOGGER.info("context getIdentityPoolId:" + context.getIdentity().getIdentityPoolId());
+		LOGGER.info("context getAwsRequestId: " + context.getAwsRequestId());
+		LOGGER.info("context getFunctionName: " + context.getFunctionName());
+		LOGGER.info("context getFunctionVersion: " + context.getFunctionVersion());
+		LOGGER.info("context getInvokedFunctionArn: " + context.getInvokedFunctionArn());
+		LOGGER.info("context getLogGroupName: " + context.getLogGroupName());
+		LOGGER.info("context getLogStreamName: " + context.getLogStreamName());
+		LOGGER.info("context getMemoryLimitInMB: " + context.getMemoryLimitInMB());
+		LOGGER.info("context getRemainingTimeInMillis: " + context.getRemainingTimeInMillis());
+		LOGGER.info("context getIdentityId: " + context.getIdentity().getIdentityId());
+		LOGGER.info("context getIdentityPoolId: " + context.getIdentity().getIdentityPoolId());
+		
+		// See: http://docs.aws.amazon.com/lambda/latest/dg/env_variables.html
+		String environmentGus = System.getenv("ENVIRONMENT_GUS");
+		LOGGER.info("ENVIRONMENT_GUS: " + environmentGus);
 
-		return new Output(input.getName(), "Eyes");
+		return new Output(input.getName(), "Eyes", "Classified");
 	}
 
 }
--- SpringJava/AWS/LAMBDA/src/main/java/de/aws/example/lambda/Output.java
+++ SpringJava/AWS/LAMBDA/src/main/java/de/aws/example/lambda/Output.java
@@ -3,14 +3,16 @@ package de.aws.example.lambda;
 public class Output {
 	private String name;
 	private String surname;
-	
+	private String rank;
+
 	protected Output() {
 		
 	}
 	
-	public Output(String name, String surname) {
+	public Output(String name, String surname, String rank) {
 		this.name = name;
 		this.surname = surname;
+		this.rank = rank;
 	}
 
 	public String getName() {
@@ -19,5 +21,9 @@ public class Output {
 	
 	public String getSurname() {
 		return surname;
+	}
+	
+	public String getRank() {
+		return rank;
 	}
 }
