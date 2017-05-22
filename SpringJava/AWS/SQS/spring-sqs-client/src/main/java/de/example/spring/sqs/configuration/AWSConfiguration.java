f98a9b4abfe685080fd52f2437d3c1102b783ef1
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2017-05-22 22:58:25 +0200

    When running in developer's PC we have no access to http://169.254.169.254

--- SpringJava/AWS/SQS/spring-sqs-client/src/main/java/de/example/spring/sqs/configuration/AWSConfiguration.java
+++ SpringJava/AWS/SQS/spring-sqs-client/src/main/java/de/example/spring/sqs/configuration/AWSConfiguration.java
@@ -6,47 +6,60 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.context.config.annotation.EnableContextInstanceData;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.example.spring.sqs.service.SenderNotificationServiceImpl;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
-// Using instance metadata: http://cloud.spring.io/spring-cloud-aws/spring-cloud-aws.html#_using_instance_metadata
-@EnableContextInstanceData
 public class AWSConfiguration {
 	private static final Logger LOGGER = LoggerFactory.getLogger(AWSConfiguration.class);
 	
-    @Value("${ami-id:N/A}")
-    private String amiId;
-
-    @Value("${hostname:N/A}")
-    private String hostname;
-
-    @Value("${instance-type:N/A}")
-    private String instanceType;
-
-    @Value("${services/domain:N/A}")
-    private String serviceDomain;
-    
-    @Value("${placement/availability-zone:N/A}")
-    private String region;
-    
 	@Value("${app.aws.sqs.queue-url}")
 	private String queueUrl;
 	
     @Bean
     SenderNotificationServiceImpl senderNotificationServiceImpl(AmazonSQSAsync amazonSqsAsync, ObjectMapper objectMapper) {
-    	LOGGER.info("amiId: " + amiId);
-    	LOGGER.info("hostname: " + hostname);
-    	LOGGER.info("instanceType: " + instanceType);
-    	LOGGER.info("serviceDomain: " + serviceDomain);
-    	LOGGER.info("region: " + region);
     	LOGGER.info("queueUrl: " + queueUrl);
     	
 		return new SenderNotificationServiceImpl(amazonSqsAsync, queueUrl, objectMapper);
     	
+    }
+
+    @Configuration
+    // When running from developer's PC: -Dspring.profiles.active=dev  (there is not access to http://169.254.169.254/ )
+    @Profile(value = "!dev")
+    // Using instance metadata: http://cloud.spring.io/spring-cloud-aws/spring-cloud-aws.html#_using_instance_metadata
+    @EnableContextInstanceData
+    public static class InstanceData {
+        @Value("${ami-id:N/A}")
+        private String amiId;
+
+        @Value("${hostname:N/A}")
+        private String hostname;
+
+        @Value("${instance-type:N/A}")
+        private String instanceType;
+
+        @Value("${services/domain:N/A}")
+        private String serviceDomain;
+
+        @Value("${placement/availability-zone:N/A}")
+        private String region;
+
+        
+        @PostConstruct
+        private void init() {
+          LOGGER.info("AWS ami id: " + amiId);
+          LOGGER.info("AWS hostname: " + hostname);
+          LOGGER.info("AWS instance type: " + instanceType);
+          LOGGER.info("AWS service domain: " + serviceDomain);
+          LOGGER.info("AWS region: " + region);
+        }
     }
 
 }
--- SpringJava/AWS/SQS/spring-sqs-server/src/main/java/de/example/spring/sqs/configuration/AWSConfiguration.java
+++ SpringJava/AWS/SQS/spring-sqs-server/src/main/java/de/example/spring/sqs/configuration/AWSConfiguration.java
@@ -1,5 +1,7 @@
 package de.example.spring.sqs.configuration;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,34 +9,17 @@ import org.springframework.cloud.aws.context.config.annotation.EnableContextInst
 import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 
 @Configuration
-//Using instance metadata: http://cloud.spring.io/spring-cloud-aws/spring-cloud-aws.html#_using_instance_metadata
-@EnableContextInstanceData
 public class AWSConfiguration {
 	private static final Logger LOGGER = LoggerFactory.getLogger(AWSConfiguration.class);
-	
-    @Value("${ami-id:N/A}")
-    private String amiId;
-
-    @Value("${hostname:N/A}")
-    private String hostname;
-
-    @Value("${instance-type:N/A}")
-    private String instanceType;
-
-    @Value("${services/domain:N/A}")
-    private String serviceDomain;
 
     @Bean
     public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory(AmazonSQSAsync amazonSqs) {
-    	LOGGER.info("amiId: " + amiId);
-    	LOGGER.info("hostname: " + hostname);
-    	LOGGER.info("instanceType: " + instanceType);
-    	LOGGER.info("serviceDomain: " + serviceDomain);
-    
+
     	SimpleMessageListenerContainerFactory factory = new SimpleMessageListenerContainerFactory();
     	factory.setAmazonSqs(amazonSqs);
     	factory.setAutoStartup(true);
@@ -42,5 +27,36 @@ public class AWSConfiguration {
 
     	return factory;
     }
+    
+    @Configuration
+    // When running from developer's PC: -Dspring.profiles.active=dev  (there is not access to http://169.254.169.254/ )
+    @Profile(value = "!dev")
+    // Using instance metadata: http://cloud.spring.io/spring-cloud-aws/spring-cloud-aws.html#_using_instance_metadata
+    @EnableContextInstanceData
+    public static class InstanceData {
+        @Value("${ami-id:N/A}")
+        private String amiId;
 
+        @Value("${hostname:N/A}")
+        private String hostname;
+
+        @Value("${instance-type:N/A}")
+        private String instanceType;
+
+        @Value("${services/domain:N/A}")
+        private String serviceDomain;
+
+        @Value("${placement/availability-zone:N/A}")
+        private String region;
+
+        
+        @PostConstruct
+        private void init() {
+          LOGGER.info("AWS ami id: " + amiId);
+          LOGGER.info("AWS hostname: " + hostname);
+          LOGGER.info("AWS instance type: " + instanceType);
+          LOGGER.info("AWS service domain: " + serviceDomain);
+          LOGGER.info("AWS region: " + region);
+        }
+    }
 }
