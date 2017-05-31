db57a45bff2885ccf4b1bb9a0d37ebd47c256ac2
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2017-06-01 01:06:14 +0200

    SQS: using custom headers

--- SpringJava/AWS/SQS/spring-sqs-client/src/main/java/de/example/spring/sqs/service/SenderNotificationServiceImpl.java
+++ SpringJava/AWS/SQS/spring-sqs-client/src/main/java/de/example/spring/sqs/service/SenderNotificationServiceImpl.java
@@ -1,7 +1,13 @@
 package de.example.spring.sqs.service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.aws.messaging.core.QueueMessageChannel;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 
@@ -12,6 +18,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import de.example.spring.sqs.service.dto.NotificationDTO;
 
 public class SenderNotificationServiceImpl {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SenderNotificationServiceImpl.class);
+	private static final String UUID_HEADER = "UUID";
+	  
 	private final QueueMessagingTemplate queueMessagingTemplate;
 	private final QueueMessageChannel queue;
 	private final ObjectMapper objectMapper;
@@ -26,7 +35,14 @@ public class SenderNotificationServiceImpl {
 	
 	public void send(NotificationDTO notificationDTO) throws JsonProcessingException {
 		String payload = objectMapper.writeValueAsString(notificationDTO);
-
-		queueMessagingTemplate.convertAndSend(queue, payload);
+		
+		String uuid = UUID.randomUUID().toString();
+		Map<String, Object> headers = new HashMap<>();
+		headers.put(UUID_HEADER, uuid);
+		
+		LOGGER.info("Header UUID");
+		LOGGER.info(uuid);
+		
+		queueMessagingTemplate.convertAndSend(queue, payload, headers);
 	}
 }
--- SpringJava/AWS/SQS/spring-sqs-server/src/main/java/de/example/spring/sqs/listener/SqsNotificationListener.java
+++ SpringJava/AWS/SQS/spring-sqs-server/src/main/java/de/example/spring/sqs/listener/SqsNotificationListener.java
@@ -1,21 +1,53 @@
 package de.example.spring.sqs.listener;
 
+import java.io.IOException;
+
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.example.spring.sqs.service.dto.NotificationDTO;
 
 @Component
 public class SqsNotificationListener {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SqsNotificationListener.class);
-	
+	private static final String UUID_HEADER = "UUID";
+
+	private final ObjectMapper objectMapper;
+
+	@Inject
+	public SqsNotificationListener(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+	}
+
 	@SqsListener(value = "${app.aws.sqs.queue-name}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-	public void queueListener(String payload) {
+	public void queueListener(String payload, @Header(name = UUID_HEADER, required = true) String uuid) {
 		// TODO: Spring should convert String payload to NotificationDTO :(
-		
+
+		NotificationDTO notificationDto;
+		try {
+			notificationDto = objectMapper.readValue(payload, NotificationDTO.class);
+		} catch (IOException ex) {
+			LOGGER.error("ObjectMapper exception: ", ex);
+
+			throw new RuntimeException(ex);
+		}
+
 		LOGGER.info("Received notification from GusQueue");
 		LOGGER.info(payload);
+		LOGGER.info(notificationDto.getName());
+		LOGGER.info(notificationDto.getSurname());
+		
+		LOGGER.info("Header UUID");
+		LOGGER.info(uuid);
+
 		LOGGER.info("End of received notification from GusQueue");
 	}
 	
