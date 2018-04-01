3cf22674bf9b5627e8d26e57a0578f3573266357
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2018-04-01 15:17:34 +0200

    WebRequest does not work with Spring Web Flux

--- SpringJava/WebReactive/spring-webreactive-resources/src/main/java/de/spring/example/rest/exception/SpringExceptionHandler.java
+++ SpringJava/WebReactive/spring-webreactive-resources/src/main/java/de/spring/example/rest/exception/SpringExceptionHandler.java
@@ -9,23 +9,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-
-import de.spring.example.rest.filter.UsernameFilter;
 
 @RestControllerAdvice
 public class SpringExceptionHandler {
-	private static final Logger LOGGER = LoggerFactory.getLogger(UsernameFilter.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringExceptionHandler.class);
 	
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(
-			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status) {
 		LOGGER.error("handleMethodArgumentNotValid: ", ex);
 		
 		return new ResponseEntity<Object>( ex.getMessage(), new HttpHeaders(), HttpStatus.UNPROCESSABLE_ENTITY);
 	}
 
 	@ExceptionHandler({ NotFoundException.class })
-	protected ResponseEntity<Object> handleResourceNotFound(final NotFoundException ex, final WebRequest request) {
+	protected ResponseEntity<Object> handleResourceNotFound(final NotFoundException ex) {
 		LOGGER.info("handleResourceNotFound: ", ex);
 		
 		return new ResponseEntity<Object>( ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND);
@@ -33,7 +30,7 @@ public class SpringExceptionHandler {
 	}
 
 	@ExceptionHandler({ Exception.class })
-	protected ResponseEntity<Object> handleApplicationException(final Exception ex, final WebRequest request) {
+	protected ResponseEntity<Object> handleApplicationException(final Exception ex) {
 		LOGGER.error("handleException: ", ex);
 	
 		return new ResponseEntity<Object>( ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
