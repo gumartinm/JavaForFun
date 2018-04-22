a44b32065b73d08f9c57f79ee46a44cfa51381e4
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2018-04-22 16:41:01 +0200

    Using flyway instead of liquibase
    I will not use H2 (better run database in container when integration tests), so there is no need of using liquibase.

--- MyBatis/SpringBoot/web-services-spring-rest-server/src/main/java/de/spring/webservices/rest/configuration/DatabaseConfiguration.java
+++ MyBatis/SpringBoot/web-services-spring-rest-server/src/main/java/de/spring/webservices/rest/configuration/DatabaseConfiguration.java
@@ -2,11 +2,10 @@ package de.spring.webservices.rest.configuration;
 
 import javax.sql.DataSource;
 
+import org.flywaydb.core.Flyway;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import liquibase.integration.spring.SpringLiquibase;
 
 @Configuration
 @MapperScan(basePackages = "de.spring.webservices.rest.persistence.repository",
@@ -14,10 +13,13 @@ import liquibase.integration.spring.SpringLiquibase;
 public class DatabaseConfiguration {
 
     @Bean
-    public SpringLiquibase liquibase(DataSource dataSource) {
-        SpringLiquibase springLiquibase = new SpringLiquibase();
-        springLiquibase.setDataSource(dataSource);
-        springLiquibase.setChangeLog("classpath:/liquibase/changeLogs.xml");
-        return springLiquibase;
+    public Flyway flyway(DataSource dataSource) {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSource);
+        flyway.setLocations("classpath:/flyway/");
+        flyway.setBaselineOnMigrate(true);
+        flyway.migrate();
+
+        return flyway;
     }
 }
---
+++ MyBatis/SpringBoot/web-services-spring-rest-server/src/main/resources/flyway/V1__LocationsClosure_DDL.sql
@@ -0,0 +1,43 @@
+create table location
+(
+    id bigint not null,
+    parent_id bigint not null,
+    description varchar(255) not null,
+    gps bytea not null
+);
+
+comment on table  location                          is 'Locations';
+comment on column location.id                       is 'Location id';
+comment on column location.parent_id                is 'Location parent id';
+comment on column location.description              is 'Location  name';
+comment on column location.gps                      is 'Location geography type';
+
+alter table location add constraint location_pk primary key ( id );
+alter table location add constraint location_fk_01 foreign key ( parent_id ) references location ( id ) on delete no action on update no action;
+alter table location alter column gps type geography(point,4326) using geography(ST_SetSRID(geometry(gps),4326));
+
+create sequence seq_location_id  start with 0 increment by 1 cache 1;
+
+
+
+
+create table location_hierarchy
+(
+    id bigint not null,
+    parent_id bigint not null,
+    child_id bigint not null,
+    level_id smallint not null
+);
+
+comment on table  location_hierarchy                is 'Locations hierarchy';
+comment on column location_hierarchy.id             is 'Location hierarchy id';
+comment on column location_hierarchy.parent_id      is 'Location hierarchy parent id';
+comment on column location_hierarchy.child_id       is 'Location hierarchy child id';
+comment on column location_hierarchy.level_id       is 'Location hierarchy level id';
+
+
+alter table location_hierarchy add constraint location_hierarchy_pk primary key ( id );
+alter table location_hierarchy add constraint location_hierarchy_fk_01 foreign key ( parent_id ) references location ( id ) on delete no action on update no action;
+alter table location_hierarchy add constraint location_hierarchy_fk_02 foreign key ( child_id ) references location ( id ) on delete no action on update no action;
+
+create sequence seq_location_hierarchy_id  start with 0 increment by 1 cache 1;
