b41fb1033c3270a330add91b3f82d7d6cf7fc72f
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2018-08-10 21:36:09 +0200

    Using multiple data sources with MyBatis and Spring Boot

--- MyBatis/SpringBoot/web-services-spring-rest-server/src/main/java/de/spring/webservices/rest/configuration/DatabaseConfiguration.java
+++ MyBatis/SpringBoot/web-services-spring-rest-server/src/main/java/de/spring/webservices/rest/configuration/DatabaseConfiguration.java
@@ -1,22 +1,52 @@
 package de.spring.webservices.rest.configuration;
 
+import javax.inject.Named;
 import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @MapperScan(basePackages = "de.spring.webservices.rest.persistence.repository",
             markerInterface = de.spring.webservices.rest.persistence.repository.BaseRepository.class)
 public class DatabaseConfiguration {
+	public static final String DATA_SOURCE_LOCATIONS = "dataSourceLocations";
+	public static final String DATA_SOURCE_CONCILIATION = "dataSourceConciliation";
+
+	@Bean(name = DATA_SOURCE_LOCATIONS)
+	@ConfigurationProperties(prefix = "datasources.locations")
+	@Primary
+	public DataSource dataSourceLocations() {
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean(name = DATA_SOURCE_CONCILIATION)
+	@ConfigurationProperties(prefix = "datasources.conciliation")
+	public DataSource dataSourceConciliation() {
+		return DataSourceBuilder.create().build();
+	}
 
     @Bean
-    public Flyway flyway(DataSource dataSource) {
+    public Flyway flywayLocations(@Named(DatabaseConfiguration.DATA_SOURCE_LOCATIONS) DataSource dataSourceLocations) {
         Flyway flyway = new Flyway();
-        flyway.setDataSource(dataSource);
-        flyway.setLocations("classpath:/db/migration/flyway/");
+        flyway.setDataSource(dataSourceLocations);
+        flyway.setLocations("classpath:/db/migration/locations/flyway/");
+        flyway.baseline();
+        flyway.migrate();
+
+        return flyway;
+    }
+
+    @Bean
+    public Flyway flywayConciliation(@Named(DatabaseConfiguration.DATA_SOURCE_CONCILIATION) DataSource dataSourceConciliation) {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSourceConciliation);
+        flyway.setLocations("classpath:/db/migration/conciliation/flyway/");
         flyway.baseline();
         flyway.migrate();
 
--- MyBatis/SpringBoot/web-services-spring-rest-server/src/test/java/de/spring/webservices/rest/configuration/DatabaseIntegrationTestConfiguration.java
+++ MyBatis/SpringBoot/web-services-spring-rest-server/src/test/java/de/spring/webservices/rest/configuration/DatabaseIntegrationTestConfiguration.java
@@ -3,21 +3,37 @@ package de.spring.webservices.rest.configuration;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DatabaseIntegrationTestConfiguration {
-    @Value("${database.port}")
-    private int port;
+	@Value("${database-locations.port}")
+    private int locationsPort;
 
-    @Bean
-    public DataSource dataSource() {
-        String jdbConnection = String.format("jdbc:postgresql://localhost:%d/locationclosure", port);
-        return DataSourceBuilder.create().username("locationclosure").password("locationclosure")
+	@Value("${database-conciliation.port}")
+    private int conciliationPort;
+
+	@Bean(name = DatabaseConfiguration.DATA_SOURCE_LOCATIONS)
+	@ConfigurationProperties(prefix = "datasources.locations")
+	@Primary
+	public DataSource dataSourceLocations() {
+        String jdbConnection = String.format("jdbc:postgresql://localhost:%d/locations", locationsPort);
+        return DataSourceBuilder.create().username("locations").password("locations")
                 .url(jdbConnection)
                 .driverClassName("org.postgresql.Driver").build();
-    }
+	}
+
+	@Bean(name = DatabaseConfiguration.DATA_SOURCE_CONCILIATION)
+	@ConfigurationProperties(prefix = "datasources.conciliation")
+	public DataSource dataSourceConciliation() {
+        String jdbConnection = String.format("jdbc:postgresql://localhost:%d/conciliation", conciliationPort);
+        return DataSourceBuilder.create().username("conciliation").password("conciliation")
+                .url(jdbConnection)
+                .driverClassName("org.postgresql.Driver").build();
+	}
 
 }
