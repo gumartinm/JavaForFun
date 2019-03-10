85d0086ff0d83edf7e817e45e18ba9ff7b7d8965
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2019-03-10 20:57:57 +0100

    Conversion is done by CustomConverter

--- Allgemeines/Debezium/src/main/java/de/example/debezium/transformer/CustomTransformer.java
+++ Allgemeines/Debezium/src/main/java/de/example/debezium/transformer/CustomTransformer.java
@@ -9,23 +9,21 @@ import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.transforms.Transformation;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Taken from https://github.com/debezium/debezium-examples/tree/master/outbox/event-routing-smt
  *
  */
 public class CustomTransformer<R extends ConnectRecord<R>> implements Transformation<R> {
+	private static final Logger logger = LoggerFactory.getLogger(CustomTransformer.class);
+
 	private static final String TOPIC = "example.customtransformer";
 	private static final String JSON_SCHEMA_URL = "https://gumartinm.name/events/customers/Customer.json/1.json";
 
-	private final ObjectMapper objectMapper = new ObjectMapper();
-
 	@Override
 	public void configure(Map<String, ?> configs) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -40,35 +38,32 @@ public class CustomTransformer<R extends ConnectRecord<R>> implements Transforma
 
 		// ignoring deletions in the events table
 		if (op.equals("d")) {
+			logger.info("Ignoring deletes");
 			return null;
-		} else if (op.equals("c") || op.equals("r")) {
-
-			Long timestamp = struct.getInt64("ts_ms");
-			OffsetDateTime published = OffsetDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.of("UTC"));
-			Struct after = struct.getStruct("after");
-			Long customerId = after.getInt64("id");
-			String firstName = after.getString("first_name");
-			String lastName = after.getString("last_name");
-			String email = after.getString("email");
-
-			Base base = Base.builder()
-					.withType("Customer")
-					.withPublished(published)
-			        .withSchema(JSON_SCHEMA_URL)
-					.build();
-			Customer customer = Customer.builder()
-					.withBase(base)
-					.withCustomerId(customerId)
-					.withFirstName(firstName)
-					.withLastName(lastName)
-					.withEmail(email)
-					.build();
-
-			String json = convertToJson(customer);
-			return record.newRecord(TOPIC, null, null, customerId, null, json, record.timestamp());
-		} else {
-			throw new IllegalArgumentException("Record of unexpected op type: " + record);
 		}
+
+		Long timestamp = struct.getInt64("ts_ms");
+		OffsetDateTime published = OffsetDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.of("UTC"));
+		Struct after = struct.getStruct("after");
+		Integer customerId = after.getInt32("id");
+		String firstName = after.getString("first_name");
+		String lastName = after.getString("last_name");
+		String email = after.getString("email");
+
+		Base base = Base.builder()
+				.withType("Customer")
+				.withPublished(published)
+		        .withSchema(JSON_SCHEMA_URL)
+				.build();
+		Customer customer = Customer.builder()
+				.withBase(base)
+				.withCustomerId(customerId)
+				.withFirstName(firstName)
+				.withLastName(lastName)
+				.withEmail(email)
+				.build();
+
+		return record.newRecord(TOPIC, null, null, customerId, null, customer, record.timestamp());
 	}
 
 	@Override
@@ -78,14 +73,5 @@ public class CustomTransformer<R extends ConnectRecord<R>> implements Transforma
 
 	@Override
 	public void close() {
-		throw new UnsupportedOperationException();
-	}
-
-	private String convertToJson(Customer customer) {
-		try {
-			return objectMapper.writeValueAsString(customer);
-		} catch (JsonProcessingException ex) {
-			throw new RuntimeException(ex);
-		}
 	}
 }
